e2e54e4c17fdcfcc0227fbdd5a67e0263e98df09
Ruslan Kuksa <ipz221_kro@student.ztu.edu.ua>
2024-02-04 04:21:26 +0200

    added gitignore and create project

---
+++ ConsoleAppSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float a, b;
+        Console.Write("Number a: ");
+        while (!float.TryParse(Console.ReadLine(), out a))
+        {
+            Console.WriteLine("Invalid input");
+        }
+        Console.Write("Number b: ");
+        while (!float.TryParse(Console.ReadLine(), out b))
+        {
+            Console.WriteLine("Invalid input");
+        }
+        Console.WriteLine($"Result: {a + b}");
+    }
+}
